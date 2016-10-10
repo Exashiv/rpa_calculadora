92c1b1c3f1338c87fa000383043a5429408bd6ef
Rafael Pacheco <rafael_algalan@hotmail.com>
2016-10-10 12:05:13 -0500

    Agrega funcionalidad y pruebas para calculadora
    Contiene el calculadora.py y los archivos lettuce
    para la generación de pruebas

---
+++ tests/features/steps.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from lettuce import step, world
+from calculadora import Calculadora
+
+@step(u'dado que tengo el número "([^"]*)" y "([^"]*)" - Arrange')
+def dado_que_tengo_el_numero_group1_y_group1_arrange(step, group1, group2):
+    cal = Calculadora()
+    world.resultado = cal.suma(int(num1),int(num2))
+
+@step(u'cuando realizo la suma - Act')
+def cuando_realizo_la_suma_act(step):
+    pass
+    
+@step(u'entonces el resultado que obtengo es "([^"]*)" - Assert')
+def entonces_el_resultado_que_obtengo_es_group1_assert(step, group1):
+    assert int(esperado)==world.resultado, 'El resultado '+esperado+' no es el esperado.'
