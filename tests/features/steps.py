6e07a4ef69b506476517a7e563656db2e2a8ab0b
Rafael Pacheco <rafael_algalan@hotmail.com>
2016-10-10 12:38:06 -0500

    Configuracion de Travis añadida
    Añadido travis.yml y requirements para la implementacion de Travis

--- tests/features/steps.py
+++ tests/features/steps.py
@@ -3,7 +3,7 @@ from lettuce import step, world
 from calculadora import Calculadora
 
 @step(u'dado que tengo el número "([^"]*)" y "([^"]*)" - Arrange')
-def dado_que_tengo_el_numero_group1_y_group1_arrange(step, group1, group2):
+def dado_que_tengo_el_numero_group1_y_group1_arrange(step, num1, num2):
     cal = Calculadora()
     world.resultado = cal.suma(int(num1),int(num2))
 
@@ -12,5 +12,5 @@ def cuando_realizo_la_suma_act(step):
     pass
     
 @step(u'entonces el resultado que obtengo es "([^"]*)" - Assert')
-def entonces_el_resultado_que_obtengo_es_group1_assert(step, group1):
+def entonces_el_resultado_que_obtengo_es_group1_assert(step, esperado):
     assert int(esperado)==world.resultado, 'El resultado '+esperado+' no es el esperado.'
